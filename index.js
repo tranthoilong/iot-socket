b092ce43a213cf484087291d422f25d57e0e474d
LongDevLor <tranthoilong@gmail.com>
2024-11-22 22:59:36 +0700

    init

--- index.js
+++ index.js
@@ -13,14 +13,38 @@ const wss = new WebSocket.Server({ server });
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/chat', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/chat', (req, res) => {
-    res.json({ message: "Welcome to the chat endpoint!" });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// app.get('/chat', (req, res) => {
+//     res.json({ message: "Welcome to the chat endpoint!" });
+// });
+
+app.get('/products', (req, res) => {
+    // Danh sách sản phẩm mẫu
+    const products = [
+        { id: 1, name: 'Sản phẩm 1', price: 100 },
+        { id: 2, name: 'Sản phẩm 2', price: 200 },
+        { id: 3, name: 'Sản phẩm 3', price: 300 },
+        { id: 4, name: 'Sản phẩm 4', price: 400 },
+        { id: 5, name: 'Sản phẩm 5', price: 500 },
+        { id: 6, name: 'Sản phẩm 6', price: 600 },
+        { id: 7, name: 'Sản phẩm 7', price: 700 },
+        { id: 8, name: 'Sản phẩm 8', price: 800 },
+        { id: 9, name: 'Sản phẩm 9', price: 900 },
+        { id: 10, name: 'Sản phẩm 10', price: 1000 },
+        { id: 11, name: 'Sản phẩm 11', price: 1100 },
+        { id: 12, name: 'Sản phẩm 12', price: 1200 },
+    ];
+
+    res.json({
+        status: 200,
+        message: 'Thành công',
+        data: products
+    });
 });
 
 const port = process.env.PORT || 3000;
---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.config = {
+    websocketUrl: "ws://localhost:3000",
+    public: "ws://iot-socket.onrender.com",
+    cpanel:"ws://l-code.top"
+};
